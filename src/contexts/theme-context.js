7a24f0712e929da887714a6ffd6f55b2cf72a53b
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-04-11 14:00:50 +0530

    feat: theme initialization from local storage

--- src/contexts/theme-context.js
+++ src/contexts/theme-context.js
@@ -1,12 +1,20 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext(null);
 
+const getTheme = () => {
+  return localStorage.getItem("np3-theme") ?? "light";
+};
+
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(getTheme);
 
   const toggleTheme = () =>
     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+
+  useEffect(() => {
+    localStorage.setItem("np3-theme", theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
